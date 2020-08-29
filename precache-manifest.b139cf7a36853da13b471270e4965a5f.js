a825af533a07c1b6fde146b7339814a232e8d44f
Roshan kumar <roshancomsc@gmail.com>
2020-08-29 12:39:26 +0530

    Updates

---
+++ precache-manifest.b139cf7a36853da13b471270e4965a5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676a640b05d16e24c32b73f2064f35ab",
+    "url": "/myait/index.html"
+  },
+  {
+    "revision": "f92084966f816ee7c340",
+    "url": "/myait/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ccf4fec0383bc59662da",
+    "url": "/myait/static/js/2.b3230f47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myait/static/js/2.b3230f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92084966f816ee7c340",
+    "url": "/myait/static/js/main.693e41db.chunk.js"
+  },
+  {
+    "revision": "2cc279a14a1e3be21719",
+    "url": "/myait/static/js/runtime-main.af3d2076.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myait/static/media/logo.5d5d9eef.svg"
+  }
+]);
