f9058ddf0a3371726f897700745e92c5a675bac8
Roshan kumar <roshancomsc@gmail.com>
2020-09-02 22:13:14 +0530

    File and folder strcture update.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Main from './Main';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
