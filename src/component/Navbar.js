7d4d10feecc14d04afc045833ec1e9f890bfacf8
Roshan kumar <roshancomsc@gmail.com>
2020-10-11 13:57:20 +0530

    some files corected

---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav>
+                This is navbar.
+                </nav>
+        )
+    }
+}
+
+export default Navbar;
